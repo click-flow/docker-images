4473161f7da75832f196426c33f784905131bd99
erik-ekberg-sg <erik@singlegrain.com>
2021-02-03 19:09:23 -0800

    Only subscribe to unique types and unique type<>arn pairs

--- lambda-hydrator/v2/index.js
+++ lambda-hydrator/v2/index.js
@@ -10,14 +10,19 @@ const stream = createEventStream({
 	username: process.env.CLOUDEVENTS_USERNAME,
 })
 
-const MAPS = JSON.parse(process.env.MAPS_JSON || JSON.stringify([]))
-const TYPES = [...new Set(MAPS.map(map => map.cloudeventType))]
+const maps = JSON.parse(process.env.MAPS_JSON || JSON.stringify([]))
+
+const types = [...new Set(maps.map(map => map.cloudeventType))]
+const uniqueMaps = maps.filter((item1, i) => {
+	const j = maps.findIndex(item2 => JSON.stringify(item1) === JSON.stringify(item2))
+	return i === j
+})
 
 const handler = async ({ cloudevent }) => {
-	const promises = MAPS
+	const promises = uniqueMaps
 		.filter(item => item.cloudeventType === cloudevent.type)
 		.map(async item => await invoke({ lambdaArn: item.lambdaArn, cloudevent }))
 
 	await Promise.allSettled(promises)
 }
-stream.listen({ handler, types: TYPES })
+stream.listen({ handler, types })
