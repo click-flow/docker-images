702f8376479e90320696b551a85337e6deaf73f8
erik-ekberg-sg <erik@singlegrain.com>
2021-04-12 15:20:45 -0700

    Rename MAPS_JSON to ARN_AND_TYPE_MAPS_JSON

--- lambda-hydrator/v3/index.js
+++ lambda-hydrator/v3/index.js
@@ -5,7 +5,7 @@ const { v5: { createCloudeventStream } } = require('@1mill/cloudevents');
 // * listen calls to the cloudevent stream.
 const maps = [
 	...new Set(
-		JSON.parse(process.env.MAPS_JSON).map(m => JSON.stringify(m))
+		JSON.parse(process.env.ARN_AND_TYPE_MAPS_JSON).map(m => JSON.stringify(m))
 	)
 ].map(m => JSON.parse(m))
 
